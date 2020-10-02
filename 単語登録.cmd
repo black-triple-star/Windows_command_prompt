9e206f954c307f1a1b78b4dc4acd495ee587bc0b
ボクはお金で動くよ <70741260+black-triple-star@users.noreply.github.com>
2020-10-02 13:39:17 +0900

    _

--- 単語登録.cmd
+++ 単語登録.cmd
@@ -4,11 +4,11 @@ rem #####################################
 chcp 65001
 chcp 932
 
+rem 単語登録の方法
 
-
-rem C:\Program Files\Common Files\Microsoft Shared\IME14\IMEJP\
-rem start /d C:\Progra~1\Common~1\Micros~1\IME14\IMEJP   /b IMJPDCT.EXE
-
-rem windows 10 64bit の場合
+rem windows 10 64bit の場合（OSによってパスが代わる）
 start /d  C:\Windows\SysWOW64\IME\IMEJP    /b IMJPDCT.EXE
 
+rem alt + T を押して、ユーザー辞書ツールを起動し
+rem alt + T T を押して、「テキストファイルからの登録」を選択
+rem 単語登録_imeのユーザー辞書.txt を選択してエンター
