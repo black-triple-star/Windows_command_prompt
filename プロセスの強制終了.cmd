cf100f71bd819fb797aee49317f20857f0151444
ボクはお金で動くよ <70741260+black-triple-star@users.noreply.github.com>
2020-10-02 13:41:35 +0900

    1st

---
+++ プロセスの強制終了.cmd
@@ -0,0 +1,37 @@
+rem #####################################
+rem 改行コードがCR+LFでないと動作しません
+rem #####################################
+chcp 65001
+chcp 932
+
+rem プロセスの強制終了
+
+@echo off
+cls
+
+echo %cd%
+echo %~dp0
+
+set EXE_NM=AutoHotkey.exe
+rem v1.1.07
+set EXE_NM=AutoHotkey_2017-0302.exe
+set EXE_NM=AutoHotkeyU64_v1.1.32.exe
+set EXE_NM=AutoHotkeyU64_v1.1.33.02.exe
+
+rem exeの名称が25byte を超えるとtasklistで表示できないので、findstr の為に、25byteでsubstringしている
+set PROCESS_NM=%EXE_NM:~0,25%
+
+echo %PROCESS_NM%
+
+
+tasklist /FI "IMAGENAME eq %EXE_NM%" | findstr /i "%PROCESS_NM%"
+if errorlevel 1 (
+	rem 起動していない
+) else (
+	rem 起動済みの場合、一度終了する。
+	taskkill /f /im "%EXE_NM%"
+)
+
+rem 起動する場合、ここを有効にする
+rem start /b %ComSpec% /c start /high %~dp0%EXE_NM%
+
