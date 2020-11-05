dda249f7d76a79f6d593ddef6888345dddb9cc9e
ボクはお金で動くよ <70741260+black-triple-star@users.noreply.github.com>
2020-11-05 13:44:20 +0900

    _

--- タイトル バーに完全なパスを表示する.cmd
+++ タイトル バーに完全なパスを表示する.cmd
@@ -6,27 +6,32 @@ chcp 932
 
 rem 「タイトル バーに完全なパスを表示する」の切り替え
 rem レジストリの編集結果は、GUIでの操作結果（エクスプローラーの「フォルダー オプション」の捜査結果）と同じだが、フルパスが表示されない
+rem OSを再起動すれば反映される
 
-rem "https://automationlabo.com/wat/?p=1954"
-rem reg add "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\CabinetState" /v "FullPath" /t REG_DWORD /d "1" /f
-rem reg add "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\CabinetState" /v "FullPath" /t REG_DWORD /d "0" /f
-
-
-@echo off
-cls
-
-set ShellFolders=HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\CabinetState
-for /f "usebackq tokens=3 delims=	 " %%i in (`reg query "%ShellFolders%" /v "FullPath"`) do (@set WHICH=%%i)
-echo %WHICH%
+reg ADD %ShellFolders% /v "FullPath" /t REG_DWORD /d "1" /f
 
 
-if "%WHICH%" == "0x1" (
-	set VALUE=0
-) else (
-	set VALUE=1
-)
-
-reg ADD %ShellFolders% /v "FullPath" /t REG_DWORD /d "%VALUE%" /f
-
-
-
+rem rem トグルできないのでここから下を実行しない。
+rem rem "https://automationlabo.com/wat/?p=1954"
+rem rem reg add "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\CabinetState" /v "FullPath" /t REG_DWORD /d "1" /f
+rem rem reg add "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\CabinetState" /v "FullPath" /t REG_DWORD /d "0" /f
+rem 
+rem 
+rem @echo off
+rem cls
+rem 
+rem set ShellFolders=HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\CabinetState
+rem for /f "usebackq tokens=3 delims=	 " %%i in (`reg query "%ShellFolders%" /v "FullPath"`) do (@set WHICH=%%i)
+rem echo %WHICH%
+rem 
+rem 
+rem if "%WHICH%" == "0x1" (
+rem 	set VALUE=0
+rem ) else (
+rem 	set VALUE=1
+rem )
+rem 
+rem reg ADD %ShellFolders% /v "FullPath" /t REG_DWORD /d "%VALUE%" /f
+rem 
+rem 
+rem 
